92c1bf3c4e0ac76a8a9e0d77f66b54812ea6de91
Janine Kayode <janine.kayode@gmail.com>
2023-12-28 21:42:24 +0000

    Update web elements

--- todolist/src/main/java/com/example/todolist/controller/WebController.java
+++ todolist/src/main/java/com/example/todolist/controller/WebController.java
@@ -1,2 +1,33 @@
-package com.example.todolist.controller;public class WebController {
+package com.example.todolist.controller;
+
+import com.example.todolist.model.Task;
+import com.example.todolist.service.TaskService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+
+@Controller
+public class WebController {
+
+    private final TaskService taskService;
+
+    @Autowired
+    public WebController(TaskService taskService) {
+        this.taskService = taskService;
+    }
+
+    @GetMapping("/")
+    public String showHomePage(Model model) {
+        model.addAttribute("tasks", taskService.getAllTasks());
+        model.addAttribute("task", new Task()); // For the form
+        return "index";
+    }
+
+    @PostMapping("/addTask")
+    public String addTask(Task task) {
+        taskService.saveTask(task);
+        return "redirect:/";
+    }
 }
